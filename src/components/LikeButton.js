2ab2735e2a287bc43642a0aac05a5d673a80b5d7
supermay <wu_jiamei@outlook.com>
2017-09-21 19:02:14 +0200

    LikeButton works now

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,15 +1,31 @@
 import React, { PureComponent } from 'react';
 
 class LikeButton extends PureComponent {
+  // default State for the button
+  constructor(){
+    super();
+    this.state = {
+      liked: false
+    }
+  }
   toggleLike = (event) => {
     event.preventDefault();
     console.log('Like Button Cliked!')
+    this.setState({liked: !this.state.liked})
   }
-  
+  // pay attention to classNames
+  classNames = () => {
+    const {liked} = this.state
+    let classes = 'like'
+    if(liked){classes += 'liked'}
+    return classes
+  }
+
   render(){
+    const {liked} = this.state
     return (
-      <p>
-        <button onClick={this.toggleLike}>LikeButton</button>
+      <p className={this.classNames()}>
+        <button onClick={this.toggleLike}>{ liked ? '❤️' : '♡' }</button>
       </p>
     )
   }
