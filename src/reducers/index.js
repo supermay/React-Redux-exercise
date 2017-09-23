cd511a3eeef58a40a77f6ab7d6440047ef00ab06
supermay <wu_jiamei@outlook.com>
2017-09-23 15:21:31 +0200

    RecipesContainer connect to the store state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,6 @@
-export default {}
+// recipes is the function, means i get the default export from reducers/recipes.js,
+// then i give this default function a name called recipes
+import recipes from './recipes'
+export default {
+  recipes
+}
